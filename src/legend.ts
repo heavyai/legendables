e712f31e3b2886b866b725f52996c5224f4a0af4
Chris Matzenbach <cmatzenbach@nvidia.com>
2026-06-02 12:09:46 -0500

    Add license headers

--- src/legend.ts
+++ src/legend.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import h from "snabbdom/h";
 import { patch } from "./vdom";
 import { VNode } from "snabbdom/vnode";
